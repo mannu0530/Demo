4ab387fec37762724c22915bcf3f05c8b57f2a2d
mannu0530 <maheshbiloniya@gmail.com>
2018-08-10 11:21:25 +0530

    type2 of gcd.py

--- gcd.py
+++ gcd.py
@@ -3,15 +3,15 @@
 def gcd(m,n):
   #let assume m >n
   if m<n :
-    (m,n)=(n,m) #swap values
+    (m,n)=(n,m)
 
   if (m%n==0):
     return n
   else:
-    return(gcd(n,m%n))
+    diff=m-n
+    return(gcd(max(n,diff),min(n,diff)))
+
 
 a=int(input("enter first number"))
 b=int(input("enter second number"))
 print "gcd of numbers is : ",gcd(a,b)
-
-      
